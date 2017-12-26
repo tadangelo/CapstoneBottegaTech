1f2ec622ff9d424bb4b06bc53d89ff3c74e221ae
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-12-25 19:54:02 -0600

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   resources :exercises
-  get 'pages/home'
 
-  get 'pages/about'
 
-  get 'pages/ruby'
+  get 'about', to: 'pages#about'
 
-  get 'pages/contact'
+  get 'ruby', to: 'pages#ruby'
+
+  get 'contact', to: 'pages#contact'
+
+  root to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
